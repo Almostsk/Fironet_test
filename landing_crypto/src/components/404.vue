61f266be39f77d2aa876a73c737e9f9582b89e07
D.Prushchak <d.prushchak@adbakers.com.ua>
2019-05-15 16:57:55 +0300

    add page 404

---
+++ landing_crypto/src/components/404.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="page-404">
+        <h1 class="text-404">The page you were looking for was moved or doesn't exist</h1>
+        <router-link to="/" class="button-404">
+            Back home
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../styles/404.scss";
+</style>
